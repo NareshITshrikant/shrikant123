b8e7a9844d6fa857f906aa6c3c31878d959e34e5
shrikant <shrikantmisal13@rediffmail.com>
2019-04-07 22:11:17 +0530

    initial commit

---
+++ src/main/java/com/nt/bo/CustomerBO.java
@@ -0,0 +1,10 @@
+package com.nt.bo;
+
+
+public class CustomerBO {
+	int custNo;
+	String custName;
+	String address;
+	String mobileNo;
+	int billAmt;
+}
---
+++ src/main/java/com/nt/dto/CustomerDTO.java
@@ -0,0 +1,8 @@
+package com.nt.dto;
+
+import java.io.Serializable;
+
+public class CustomerDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
